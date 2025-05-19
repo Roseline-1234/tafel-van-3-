d2c2bdef0c3ba67ef71c2e72b1ff48ef5175023e
Roseline-1234 <roselineackaert@gmail.com>
2025-05-19 10:27:02 +0200

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,3 +1,13 @@
+let getal = 0
 basic.forever(function () {
-	
+    if (getal < 12) {
+        basic.showNumber(0)
+        basic.showNumber(3)
+        basic.showNumber(6)
+        basic.showNumber(9)
+        basic.showNumber(12)
+        basic.pause(1000)
+    } else {
+        getal += 0
+    }
 })
